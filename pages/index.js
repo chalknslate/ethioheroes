ba2110717932d51f3abe701d788a7258d2642c7f
chalknslate <calebecoder@gmail.com>
2025-07-24 14:31:09 -0700

    Stuff.

--- pages/index.js
+++ pages/index.js
@@ -15,35 +15,37 @@ export default function Home() {
         <title>Ethio Heroes</title>
       </Head>
       <main>
-        <Header title="Welcome to Ethio Heroes!" />
-        <Subheader title = "Create a persona below."/>
-                <form onSubmit={async (e) => {
-          e.preventDefault();
-          const formData = {
-            fname: e.target.fname.value,
-            lname: e.target.lname.value
-          };
+        <div class="wrapper">
+          <div class="wave"></div>
+        </div>
+        <div className="box">
+          <Header title="Welcome to Ethio Heroes!" />
+          <Subheader title = "Create a persona below."/>
+                  <form onSubmit={async (e) => {
+            e.preventDefault();
+            const formData = {
+              fname: e.target.fname.value,
+              lname: e.target.lname.value
+            };
 
-          const res = await fetch('/.netlify/functions/create-user', {
-            method: 'POST',
-            body: JSON.stringify(formData)
-          });
+            const res = await fetch('/.netlify/functions/create-user', {
+              method: 'POST',
+              body: JSON.stringify(formData)
+            });
 
-          const data = await res.json();
-          alert(data.message || data.error);
-        }}>
-          <label htmlFor="fname">Character Name:</label><br />
-          <input type="text" id="fname" name="fname" required /><br />
+            const data = await res.json();
+            alert(data.message || data.error);
+          }}>
+            <label htmlFor="fname">Character Name:</label><br />
+            <input type="text" id="fname" name="fname" required /><br />
 
-          <label htmlFor="lname">Password:</label><br />
-          <input type="password" id="lname" name="lname" required /><br />
+            <label htmlFor="lname">Password:</label><br />
+            <input type="password" id="lname" name="lname" required /><br />
 
-          <input type="submit" value="Submit" />
-        </form>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </main>
-      <div class="wrapper">
-        <div class="wave"></div>
-      </div>
         
     </div>
   )
