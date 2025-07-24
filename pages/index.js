cfbffbfbc1093a1a1c456c53d7770fadb8464235
chalknslate <calebecoder@gmail.com>
2025-07-23 21:58:47 -0700

    e.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-//import Footer from '@components/Footer'
+// import Footer from '@components/Footer'
 import { neon } from '@netlify/neon';
+
 const sql = neon();
+
 export default function Home() {
   return (
-    
     <div className="container">
       <Head>
         <title>Ethio Heroes</title>
@@ -13,14 +14,15 @@ export default function Home() {
       <main>
         <Header title="Welcome to Ethio Heroes!" />
         <form action="/action_page.php">
-          <label for="fname">First name:</label>
-          <input type="text" id="fname" name="fname" value="John"></input>
-          <label for="lname">Last name:</label>
-          <input type="text" id="lname" name="lname" value="Doe"></input>
-          <input type="submit" value="Submit"></input>
+          <label htmlFor="fname">First name:</label>
+          <input type="text" id="fname" name="fname" defaultValue="John" />
+          
+          <label htmlFor="lname">Last name:</label>
+          <input type="text" id="lname" name="lname" defaultValue="Doe" />
+          
+          <input type="submit" value="Submit" />
         </form>
       </main>
     </div>
-    
   )
 }
