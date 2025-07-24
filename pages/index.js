acafc4235515a242de016180805c977860330b8d
chalknslate <calebecoder@gmail.com>
2025-07-24 15:18:27 -0700

    sds

--- pages/index.js
+++ pages/index.js
@@ -14,31 +14,31 @@ export default function Home() {
           <div className="drag-handle"> 
             <Header title="Welcome to Ethio Heroes!" />
             <Subheader title="Create a persona below." />
+            <form onSubmit={async (e) => {
+              e.preventDefault();
+              const formData = {
+                fname: e.target.fname.value,
+                lname: e.target.lname.value
+              };
+
+              const res = await fetch('/.netlify/functions/create-user', {
+                method: 'POST',
+                body: JSON.stringify(formData)
+              });
+
+              const data = await res.json();
+              alert(data.message || data.error);
+            }}>
+              <label htmlFor="fname">Character Name:</label><br />
+              <input type="text" id="fname" name="fname" required /><br />
+
+              <label htmlFor="lname">Password:</label><br />
+              <input type="password" id="lname" name="lname" required /><br />
+              <div className="button">
+                <button type="submit">Create Character</button>
+              </div>
+            </form>
           </div>
-          <form onSubmit={async (e) => {
-            e.preventDefault();
-            const formData = {
-              fname: e.target.fname.value,
-              lname: e.target.lname.value
-            };
-
-            const res = await fetch('/.netlify/functions/create-user', {
-              method: 'POST',
-              body: JSON.stringify(formData)
-            });
-
-            const data = await res.json();
-            alert(data.message || data.error);
-          }}>
-            <label htmlFor="fname">Character Name:</label><br />
-            <input type="text" id="fname" name="fname" required /><br />
-
-            <label htmlFor="lname">Password:</label><br />
-            <input type="password" id="lname" name="lname" required /><br />
-            <div className="button">
-              <button type="submit">Create Character</button>
-            </div>
-          </form>
         </main>
       </div>
     </Draggable>
